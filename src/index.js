8b7ebdec685e3a60c87dc5f48ff07e73b8c7b374
mattheww <matthew.would@derivco.co.za>
2019-02-15 14:11:26 +0200

    Add an alternative method that allows access to the scene instances.

--- src/index.js
+++ src/index.js
@@ -2,11 +2,30 @@ import { Application } from 'pixi.js';
 import { load } from 'pixi-animate';
 
 const square = require('./square'),
- circle = require('./circle'),
- app = new Application();
+    circle = require('./circle'),
+    app = new Application();
 
- document.body.appendChild(app.view); 
+document.body.appendChild(app.view);
 
-load(square.stage, app.stage);
-load(circle.stage, app.stage);
+_useLoadUtil();
+// _accessInstances();
 
+// If you have your own stage, you can bypass the Scene and just use the pixi-animate load function in isolation
+function _useLoadUtil() {
+    load(square.stage, app.stage);
+    load(circle.stage, app.stage);
+}
+
+// If you want to be in control of the display objects, you can again use the load function, 
+// create a stage instance onComplete and addChild to wherever you need it. The pixi-animate 'stage' is a type of MovieClip.
+function _accessInstances() {
+    load(circle.stage).onComplete.once(() => {
+        const circleStage = new circle.stage();
+        app.stage.addChild(circleStage);
+    });
+
+    load(square.stage).onComplete.once(() => {
+        const squareStage = new square.stage();
+        app.stage.addChild(squareStage);
+    });
+}
